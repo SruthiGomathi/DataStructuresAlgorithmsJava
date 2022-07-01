e30a62fe6770cc3d21c50fd11ec0227e12711623
Sruthi Gomathi <86158320+SruthiGomathi@users.noreply.github.com>
2022-07-01 20:10:44 +0530

    Sorting Algorithms
    Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in the wrong order. This algorithm is not suitable for large data sets as its average and worst case time complexity is quite high.

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,3 +1,8 @@
+/*Sorting Algorithms - Bubble Sort
+
+Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in the wrong order. 
+This algorithm is not suitable for large data sets as its average and worst case time complexity is quite high. */
+
 package DataStructureProblems;
 
 import java.util.Scanner;
